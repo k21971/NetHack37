416de4da9be3a1557180d233d1398bca634ded6a
k21971 <keith.simpson1971@gmail.com>
2023-01-20 02:23:52 +0000

    Update install script/hints file for new editlevel version (70).

--- install-to-chroot.sh
+++ install-to-chroot.sh
@@ -17,9 +17,9 @@ COMPRESSBIN="/bin/gzip"
 # fixed data to copy (leave blank to skip)
 NH_GIT="/home/build/NetHack37"
 # HACKDIR from include/config.h; aka nethack subdir inside chroot
-NHSUBDIR="nh370.69-hdf"
+NHSUBDIR="nh370.70-hdf"
 # VAR_PLAYGROUND from include/unixconf.h
-NH_VAR_PLAYGROUND="/nh370.69-hdf/var/"
+NH_VAR_PLAYGROUND="/nh370.70-hdf/var/"
 # END OF CONFIG
 ##############################################################################
 
@@ -44,8 +44,8 @@ set -e
 umask 022
 
 echo "Creating inprogress and extrainfo directories"
-mkdir -p "$NAO_CHROOT/dgldir/inprogress-nh370.69-hdf"
-chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-nh370.69-hdf"
+mkdir -p "$NAO_CHROOT/dgldir/inprogress-nh370.70-hdf"
+chown "$USRGRP" "$NAO_CHROOT/dgldir/inprogress-nh370.70-hdf"
 mkdir -p "$NAO_CHROOT/dgldir/extrainfo-nh370"
 chown "$USRGRP" "$NAO_CHROOT/dgldir/extrainfo-nh370"
 
