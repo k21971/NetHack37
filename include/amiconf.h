aa25005cf027c47a1f3dc97e5ef6087310656f94
nhmall <nhmall@nethack.org>
2026-03-31 12:57:41 -0400

    whitespace cleanup on recent file
    replace leading tab with spaces to match include style

--- include/amiconf.h
+++ include/amiconf.h
@@ -193,21 +193,21 @@ void amii_setpens(int);
 #define M(c) ((c) -128)
 #endif
 struct ami_sysflags {
-	char sysflagsid[10];
+    char sysflagsid[10];
 #ifdef AMIFLUSH
-	boolean altmeta;  /* use ALT keys as META */
-	boolean amiflush; /* kill typeahead */
+    boolean altmeta;  /* use ALT keys as META */
+    boolean amiflush; /* kill typeahead */
 #endif
 #ifdef AMII_GRAPHICS	        
-	int numcols;
-	unsigned short amii_dripens[20]; /* DrawInfo Pens currently there are 13 in v39 */
-	AMII_COLOR_TYPE amii_curmap[AMII_MAXCOLORS]; /* colormap */
+    int numcols;
+    unsigned short amii_dripens[20]; /* DrawInfo Pens currently there are 13 in v39 */
+    AMII_COLOR_TYPE amii_curmap[AMII_MAXCOLORS]; /* colormap */
 #endif
 #ifdef OPT_DISPMAP
-	boolean fast_map; /* use optimized, less flexible map display */
+    boolean fast_map; /* use optimized, less flexible map display */
 #endif
 #ifdef MFLOPPY
-	boolean asksavedisk;
+    boolean asksavedisk;
 #endif
 };
 extern struct ami_sysflags sysflags;
