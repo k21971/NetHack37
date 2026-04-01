3b4d4cd261a2b82eb972f06c06b04f1717b34007
nhmall <nhmall@nethack.org>
2026-04-01 12:56:37 -0400

    expand range of oc_weight

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 135
+#define EDITLEVEL 136
 
 /*
  * Development status possibilities.
